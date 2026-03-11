11c471abd07bf90d547b30ec70e287d9175052ef
João Gonçalves <joao.goncalves@vectalis.com>
2026-03-11 11:21:35 +0000

    Spring Boot project with base dependencies

---
+++ src/test/java/pt/rho/exchange_rate_api/ExchangeRateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.rho.exchange_rate_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/pt/rho/exchangerate/ExchangeRateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.rho.exchangerate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
